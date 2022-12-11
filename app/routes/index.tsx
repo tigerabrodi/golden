489d5d1f213c7b7d57b37f073d2f99fc88f47dcb
tigerabrodi <tigerabrodi@gmail.com>
2022-12-11 07:54:59 +0100

    implement sass

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,11 @@
+import type { LinksFunction } from '@remix-run/node'
+
+import styles from './index.css'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
+
 export default function Index() {
   return <div>hello world</div>
 }
