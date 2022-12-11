e8c104f577c4b2dadba7f8a59c2535b20eadf60e
tigerabrodi <tigerabrodi@gmail.com>
2022-12-11 06:47:23 +0100

    feat: implement fonts and ico

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,10 @@
-import type { MetaFunction } from '@remix-run/node'
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
 
+import RobotoMono500 from '@fontsource/roboto-mono/500.css'
+import Roboto400 from '@fontsource/roboto/400.css'
+import Roboto500 from '@fontsource/roboto/500.css'
+import Roboto600 from '@fontsource/roboto/600.css'
+import Roboto700 from '@fontsource/roboto/700.css'
 import {
   Links,
   LiveReload,
@@ -14,6 +19,16 @@ export const meta: MetaFunction = () => ({
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 })
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: Roboto400 },
+    { rel: 'stylesheet', href: Roboto500 },
+    { rel: 'stylesheet', href: Roboto600 },
+    { rel: 'stylesheet', href: Roboto700 },
+    { rel: 'stylesheet', href: RobotoMono500 },
+  ]
+}
 
 export default function App() {
   return (
