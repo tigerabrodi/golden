af974d9cb72af12e682a2cc3836720b9e0a0b3f2
tigerabrodi <tigerabrodi@gmail.com>
2022-12-12 09:18:48 +0100

    feat: implement login UI

---
+++ app/routes/login.tsx
@@ -0,0 +1,34 @@
+import type { LinksFunction } from '@remix-run/node'
+
+import styles from './auth.css'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
+
+export default function Login() {
+  return (
+    <main className="auth">
+      <h1>Login</h1>
+      <p>Fun seeing you’re back!</p>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="john@gmail.com"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  )
+}
