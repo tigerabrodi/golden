9ced42cce9ddcb5e0e8dd1d92942f4b55976fda1
tigerabrodi <tigerabrodi@gmail.com>
2022-12-13 07:16:26 +0100

    feat: logout resource route

---
+++ app/routes/logout.ts
@@ -0,0 +1,32 @@
+import type { ActionFunction } from '@remix-run/node'
+
+import { redirect } from '@remix-run/node'
+
+import { authDestroySession, authGetSession } from '~/sessions/auth.server'
+import {
+  validationCommitSession,
+  validationGetSession,
+} from '~/sessions/validationStates.server'
+import { SET_COOKIE, VALIDATION_STATE_SUCCESS } from '~/types'
+import { getCookie } from '~/utils/getCookie'
+
+export const action: ActionFunction = async ({ request }) => {
+  const [authSession, validationSession] = await Promise.all([
+    authGetSession(getCookie(request)),
+    validationGetSession(getCookie(request)),
+  ])
+
+  validationSession.flash(VALIDATION_STATE_SUCCESS, 'Successfully logged out.')
+
+  const [newAuthCookie, validationCommitedSession] = await Promise.all([
+    authDestroySession(authSession),
+    validationCommitSession(validationSession),
+  ])
+
+  return redirect('/', {
+    headers: [
+      [SET_COOKIE, newAuthCookie],
+      [SET_COOKIE, validationCommitedSession],
+    ],
+  })
+}
