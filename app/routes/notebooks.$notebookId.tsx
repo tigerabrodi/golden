48110677ab03aba43eff938cbb0e30c8332d84b9
tigerabrodi <tigerabrodi@gmail.com>
2022-12-14 10:05:06 +0100

    feat: implement notebook empty ui

--- app/routes/notebooks.$notebookId.tsx
+++ app/routes/notebooks.$notebookId.tsx
@@ -1,3 +1,40 @@
+import type { LinksFunction } from '@remix-run/node'
+
+import { Form, Link, Outlet } from '@remix-run/react'
+
+import styles from './notebooks.$notebookId.css'
+
+import { AddPen, Delete, PenWithPaper } from '~/icons'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
+
 export default function Notebook() {
-  return <h1>All notes</h1>
+  return (
+    <>
+      <div className="notes">
+        <div className="header">
+          <h1>General notes</h1>
+          <Form method="post">
+            <button type="submit">
+              <AddPen />
+            </button>
+          </Form>
+
+          <Link to="/">
+            <Delete />
+          </Link>
+        </div>
+
+        <div className="content">
+          <div>
+            <PenWithPaper />
+            <h2>No notes.</h2>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
 }
