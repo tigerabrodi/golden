4fdadb3f145edfb6cf16b8667c6a3e3c44f0c785
tigerabrodi <tigerabrodi@gmail.com>
2022-12-14 10:04:14 +0100

    feat: notebooks page

--- app/routes/notebooks.tsx
+++ app/routes/notebooks.tsx
@@ -1,10 +1,41 @@
-import { Outlet } from '@remix-run/react'
+import type { LinksFunction } from '@remix-run/node'
+
+import { Link, Outlet } from '@remix-run/react'
+
+import styles from './notebooks.css'
+
+import { Book, Paper, Plus } from '~/icons'
+import { ALL_NOTES, NOTEBOOKS } from '~/types'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export default function Notebooks() {
   return (
-    <div>
-      hello world
+    <main>
+      <div className="notebooks">
+        <Link to={`/${NOTEBOOKS}/${ALL_NOTES}`} className="all-notes-link">
+          <Paper />
+          <span>All notes</span>
+        </Link>
+
+        <div className="notebooks-list">
+          <div>
+            <Book />
+            <p>Notebooks</p>
+            <Link to={`/${NOTEBOOKS}/add`}>
+              <Plus />
+            </Link>
+          </div>
+          <ul>
+            <Link to={`/${NOTEBOOKS}/notebook-1`}>General notes</Link>
+            <Link to={`/${NOTEBOOKS}/notebook-1`}>General notes</Link>
+            <Link to={`/${NOTEBOOKS}/notebook-1`}>General notes</Link>
+          </ul>
+        </div>
+      </div>
       <Outlet />
-    </div>
+    </main>
   )
 }
