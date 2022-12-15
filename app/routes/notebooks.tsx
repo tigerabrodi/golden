42b03523dc077c8987257664cf0b814fb64ddbdf
tigerabrodi <tigerabrodi@gmail.com>
2022-12-15 10:02:16 +0100

    feat: notebooks logic

--- app/routes/notebooks.tsx
+++ app/routes/notebooks.tsx
@@ -1,17 +1,63 @@
-import type { LinksFunction } from '@remix-run/node'
+import type { DataFunctionArgs, LinksFunction } from '@remix-run/node'
 
-import { Link, Outlet } from '@remix-run/react'
+import { json } from '@remix-run/node'
+import { redirect } from '@remix-run/node'
+import { Link, Outlet, useLoaderData } from '@remix-run/react'
 
 import styles from './notebooks.css'
 
+import { getServerFirebase } from '~/firebase'
+import { getUserNotebooks } from '~/firebase/read.server'
 import { Book, Paper, Plus } from '~/icons'
-import { ALL_NOTES, NOTEBOOKS } from '~/types'
+import { authGetSession } from '~/sessions/auth.server'
+import {
+  validationCommitSession,
+  validationGetSession,
+} from '~/sessions/validationStates.server'
+import {
+  ACCESS_TOKEN,
+  ALL_NOTES,
+  NOTEBOOKS,
+  SET_COOKIE,
+  VALIDATION_STATE_ERROR,
+} from '~/types'
+import { getCookie } from '~/utils/getCookie'
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }]
 }
 
+export const loader = async ({ request }: DataFunctionArgs) => {
+  const { firebaseAdminAuth } = getServerFirebase()
+
+  const [authSession, validationSession] = await Promise.all([
+    authGetSession(getCookie(request)),
+    validationGetSession(getCookie(request)),
+  ])
+
+  const token = authSession.get(ACCESS_TOKEN)
+
+  try {
+    const { uid: ownerId } = await firebaseAdminAuth.verifySessionCookie(token)
+    const notebooks = await getUserNotebooks(ownerId)
+    return json({ notebooks })
+  } catch (error) {
+    validationSession.flash(
+      VALIDATION_STATE_ERROR,
+      'You need to be logged in to access this page.'
+    )
+
+    return redirect('/', {
+      headers: {
+        [SET_COOKIE]: await validationCommitSession(validationSession),
+      },
+    })
+  }
+}
+
 export default function Notebooks() {
+  const { notebooks } = useLoaderData<typeof loader>()
+
   return (
     <main>
       <div className="notebooks">
@@ -28,11 +74,15 @@ export default function Notebooks() {
               <Plus />
             </Link>
           </div>
-          <ul>
-            <li>
-              <Link to={`/${NOTEBOOKS}/notebook-1`}>General notes</Link>
-            </li>
-          </ul>
+          {notebooks.length > 0 && (
+            <ul>
+              {notebooks.map(({ id, name }) => (
+                <li key={id}>
+                  <Link to={`/${NOTEBOOKS}/${id}`}>{name}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
       <Outlet />
