031ba506744ac567acf2e86d5c2b85abdaab0e2d
tigerabrodi <tigerabrodi@gmail.com>
2022-12-16 07:02:58 +0100

    feat: add note schema

--- app/types/firebase.ts
+++ app/types/firebase.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+import { CREATED_AT, OWNER_ID } from '~/firebase'
+
 export const TimestampSchema = z.object({
   seconds: z.number(),
   nanoseconds: z.number(),
@@ -10,11 +12,21 @@ export type Timestamp = z.infer<typeof TimestampSchema>
 export const NotebookSchema = z.object({
   id: z.string(),
   name: z.string(),
-  ownerId: z.string(),
-  createdAt: TimestampSchema,
+  [OWNER_ID]: z.string(),
+  [CREATED_AT]: TimestampSchema,
 })
 
 export type Notebook = z.infer<typeof NotebookSchema>
+
+export const NoteSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  text: z.string(),
+  [OWNER_ID]: z.string(),
+  [CREATED_AT]: TimestampSchema,
+})
+
+export type Note = z.infer<typeof NoteSchema>
 
 export const FirebaseOptionsSchema = z.object({
   apiKey: z.string().optional(),
