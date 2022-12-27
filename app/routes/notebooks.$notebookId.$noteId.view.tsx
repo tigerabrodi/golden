39a547fd5a9f2e93e69aabc60d2c2f871b8cf9a7
tigerabrodi <tigerabrodi@gmail.com>
2022-12-27 07:06:55 +0100

    feat: implement view of markdown

--- app/routes/notebooks.$notebookId.$noteId.view.tsx
+++ app/routes/notebooks.$notebookId.$noteId.view.tsx
@@ -1,6 +1,12 @@
-import type { loader as noteLoader } from './notebooks.$notebookId.$noteId'
+import type {
+  loader as noteLoader,
+  NoteOutletContext,
+} from './notebooks.$notebookId.$noteId'
 
-import { Link } from '@remix-run/react'
+import { Link, useOutletContext } from '@remix-run/react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import { useLoaderRouteData } from '~/hooks'
 import { Delete, EditPen } from '~/icons'
@@ -11,12 +17,14 @@ export default function Note() {
     'routes/notebooks.$notebookId.$noteId'
   )
 
+  const { note } = useOutletContext<NoteOutletContext>()
+
   return noteLoaderData ? (
     <>
       <div className="header">
-        <h2>{noteLoaderData.initialNote.name}</h2>
+        <h2>{note.name}</h2>
         <Link
-          to={`/${NOTEBOOKS}/${noteLoaderData.notebookId}/${noteLoaderData.initialNote.id}/edit`}
+          to={`/${NOTEBOOKS}/${noteLoaderData.notebookId}/${note.id}/edit`}
           className="edit-view"
           prefetch="intent"
         >
@@ -25,7 +33,7 @@ export default function Note() {
         </Link>
 
         <Link
-          to={`/${NOTEBOOKS}/${noteLoaderData.notebookId}/${noteLoaderData.initialNote.id}/view/delete`}
+          to={`/${NOTEBOOKS}/${noteLoaderData.notebookId}/${note.id}/view/delete`}
           className="delete"
           prefetch="intent"
         >
@@ -34,7 +42,30 @@ export default function Note() {
         </Link>
       </div>
 
-      <div />
+      <div className="markdown">
+        <ReactMarkdown
+          children={note.content}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '')
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  // @ts-ignore
+                  style={nord}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              )
+            },
+          }}
+        />
+      </div>
     </>
   ) : null
 }
