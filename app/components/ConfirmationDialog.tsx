f27c725db7e189ba087261c064198d35f74133c3
tigerabrodi <tigerabrodi@gmail.com>
2023-01-03 07:11:47 +0100

    feat: confirmation dialog

--- app/components/ConfirmationDialog.tsx
+++ app/components/ConfirmationDialog.tsx
@@ -1,35 +1,17 @@
-import { Dialog } from '@headlessui/react'
-import { Form, Link, useNavigate } from '@remix-run/react'
+import { Form, Link } from '@remix-run/react'
 
-import { Close } from '~/icons'
+import { Dialog } from './Dialog'
+
 import { BACK_ROUTE } from '~/types'
 
 export function ConfirmationDialog({ title }: { title: string }) {
-  const navigate = useNavigate()
-
   return (
-    <>
-      <Dialog
-        open
-        onClose={() => navigate(BACK_ROUTE)}
-        className="confirmation-dialog"
-      >
-        <div className="backdrop" aria-hidden="true" />
+    <Dialog title={title}>
+      <Form method="post">
+        <Link to={BACK_ROUTE}>Cancel</Link>
 
-        <Dialog.Panel className="panel">
-          <Link to={BACK_ROUTE} className="close">
-            <Close />
-          </Link>
-
-          <Dialog.Title as="h1">{title}</Dialog.Title>
-
-          <Form className="actions" method="post">
-            <Link to={BACK_ROUTE}>Cancel</Link>
-
-            <button type="submit">Delete</button>
-          </Form>
-        </Dialog.Panel>
-      </Dialog>
-    </>
+        <button type="submit">Delete</button>
+      </Form>
+    </Dialog>
   )
 }
